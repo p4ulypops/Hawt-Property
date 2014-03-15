c672af262cfd3d3850ab2e151597c2c29001ed49
Paul Silver <Paul.silver@images.co.uk>
2014-03-15 12:13:39 +0000

    $_DATA array container for views
    This allows for me to get all the vars in a $_DATA array

---
+++ application/core/MY_loader.php
@@ -0,0 +1,28 @@
+<?php  if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+
+class MY_Loader extends CI_Loader
+{
+	
+	 function __construct()
+    {
+        parent::__construct();
+    }
+
+	
+	// Now you can use $_DATA to get all the view's vars.
+	function view($view, $vars = array(), $return = FALSE) {
+		
+		$outputVars = $this->_ci_object_to_array($vars);
+		if (is_array($outputVars)) {
+			foreach($outputVars as $k => $v) {
+				$outputVars['_DATA'][$k] = $v;	
+			}
+		}
+		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $outputVars, '_ci_return' => $return));
+	}
+	
+	
+}
