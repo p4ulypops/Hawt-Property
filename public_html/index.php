9ed3cf7fa4f10551fdab2994e730a9bb00fde829
Paul Silver <Paul.silver@images.co.uk>
2014-03-15 11:50:00 +0000

    Paths

--- public_html/index.php
+++ public_html/index.php
@@ -185,7 +185,7 @@ if (defined('ENVIRONMENT'))
 	{
 		if ( ! is_dir(BASEPATH.$application_folder.'/'))
 		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF." =-=-=-= \"".BASEPATH.$application_folder.'/"');
 		}
 
 		define('APPPATH', BASEPATH.$application_folder.'/');
