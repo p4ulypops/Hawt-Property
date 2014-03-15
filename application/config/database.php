48f5a364d129d07971accad5e4aa63be286398e7
Paul Silver <Paul.silver@images.co.uk>
2014-03-15 11:10:57 +0000

    DB Settings

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'paulgd_hawtprop';
+$db['default']['password'] = 'hawtprop';
+$db['default']['database'] = 'paulgd_hawtprop';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
